34e79ae1e192aba200de0f8603134e04dd502d35
Jordan Neumann <jsneum01@louisville.edu>
2021-03-30 19:21:50 -0400

    Add project files.

---
+++ RollD20CodeLouisville/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using RollD20WindowsForm;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public List<Dice> RollResult;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+        }
+
+        public void DiceRollsFormFiller()
+        {
+            foreach (Dice die in RollResult)
+            {
+                chart1.Series["NumberOfDiceRolls"].Points.Add(die.Result);
+            }
+            chart1.Titles.Add("Number Of Dice Rolls Per Number");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
